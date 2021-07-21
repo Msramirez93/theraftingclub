db53d81ebac8fd433849796ef84979f4c8ed3288
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-07-21 20:04:41 -0300

    arreglando faq

--- js/date.js
+++ js/date.js
@@ -2,9 +2,13 @@ const yearElement = document.querySelector("#date");
 yearElement.innerText = new Date().toLocaleDateString('en-GB', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
 /*last mood*/
-function toggleMenu() {
+//function toggleMenu() {
     // console.log(document.getElementById("primaryNav").class);
-    document.getElementById("primaryNav").classList.toggle("hide");
+    //document.getElementById("primaryNav").classList.toggle("hide");
+//}
+function toggleMenu() {
+	console.log(document.getElementById("menu").classList);
+	document.getElementById("menu").classList.toggle("hide");
 }
 document.querySelector('#lastmod').textContent = document.lastModified;
 // for current date in the footer
--- js/gallery.js
+++ js/gallery.js
@@ -1,33 +1,34 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    
-    };
-};
-const imgOptions = {
-    rootMargin: '0px 0px 50px 0px',
-    threshold: 1
-};
-
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver(items => {
-        items.forEach(item => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-
-            }
-        });
-    }, imgOptions);
-
-    imagesToLoad.forEach(img => {
-        imgObserver.observe(img);
+  //lazy load img//
+  const imagesToLoad = document.querySelectorAll('img[data-src]');
+  const loadImages = (image) => {
+      image.setAttribute('src', image.getAttribute('data-src'));
+      image.onload = () => {
+          image.removeAttribute('data-src');
+      
+      };
+  };
+  const imgOptions = {
+      rootMargin: '0px 0px 50px 0px',
+      threshold: 1
+  };
+  
+  if ('IntersectionObserver' in window) {
+      const imgObserver = new IntersectionObserver(items => {
+          items.forEach(item => {
+              if (item.isIntersecting) {
+                  loadImages(item.target);
+                  imgObserver.unobserve(item.target);
+  
+              }
+          });
+      }, imgOptions);
+  
+      imagesToLoad.forEach(img => {
+          imgObserver.observe(img);
+      });
+  } else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
     });
-} else {
-  imagesToLoad.forEach((img) => {
-      loadImages(img);
-  });
-}
-/*WebFont.load({google: {families: [ "Benne", serif]}});*/
+  }
+  //WebFont.load({google: {families: ['Acme']}});
