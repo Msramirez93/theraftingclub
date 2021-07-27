56df5e6fccb8d75309b997fb040aef4863c628ac
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-07-27 19:27:47 -0300

    modificando el sitio

--- js/date.js
+++ js/date.js
@@ -1,25 +1,4 @@
 const yearElement = document.querySelector("#date");
 yearElement.innerText = new Date().toLocaleDateString('en-GB', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
-/*last mood*/
-//function toggleMenu() {
-    // console.log(document.getElementById("primaryNav").class);
-    //document.getElementById("primaryNav").classList.toggle("hide");
-//}
-function toggleMenu() {
-	console.log(document.getElementById("menu").classList);
-	document.getElementById("menu").classList.toggle("hide");
-}
 document.querySelector('#lastmod').textContent = document.lastModified;
-// for current date in the footer
-/*const yearElement = document.querySelector("#date");*/
-/*yearElement.innerText = new Date().toLocaleDateString('en-GB', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });*/
-
-
-// for the banner
-const banner = document.querySelector('.banner');
-const dayOfWeek = new Date().getDay();
-if (dayOfWeek === 5) {
-    banner.classList.add('visible');
-}
- //WebFont.load({google: {families: ['Merriweather']}}); 
